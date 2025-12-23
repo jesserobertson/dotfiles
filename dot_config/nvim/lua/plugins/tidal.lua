e3a61e41a694f1420656d1e88fe1cef35cc0084c
Jess Robertson <jess.robertson@niwa.co.nz>
2025-12-24 09:34:54 +1300

    Adding latest changes

--- dot_config/nvim/lua/plugins/tidal.lua
+++ dot_config/nvim/lua/plugins/tidal.lua
@@ -1,29 +1,49 @@
 -- TidalCycles live coding plugin
 return {
-  "tidalcycles/vim-tidal",
-  ft = "tidal", -- Load only for .tidal files
-  keys = {
-    { "<C-e>", desc = "Send paragraph to TidalCycles", mode = { "n", "v" }, ft = "tidal" },
-    { "<localleader>ss", desc = "Send inner paragraph to TidalCycles", mode = "n", ft = "tidal" },
-    { "<localleader>s", desc = "Send line/selection to TidalCycles", mode = { "n", "v" }, ft = "tidal" },
-    { "<localleader>h", desc = "Silence all TidalCycles streams", mode = "n", ft = "tidal" },
-  },
-  init = function()
-    -- Set localleader for TidalCycles commands
-    vim.g.maplocalleader = ","
+    "tidalcycles/vim-tidal",
+    ft = "tidal", -- Load only for .tidal files
+    keys = {
+        {
+            "<C-e>",
+            desc = "Send paragraph to TidalCycles",
+            mode = { "n", "v" },
+            ft = "tidal",
+        },
+        {
+            "<localleader>ss",
+            desc = "Send inner paragraph to TidalCycles",
+            mode = "n",
+            ft = "tidal",
+        },
+        {
+            "<localleader>s",
+            desc = "Send line/selection to TidalCycles",
+            mode = { "n", "v" },
+            ft = "tidal",
+        },
+        {
+            "<localleader>h",
+            desc = "Silence all TidalCycles streams",
+            mode = "n",
+            ft = "tidal",
+        },
+    },
+    init = function()
+        -- Set localleader for TidalCycles commands
+        -- vim.g.maplocalleader = ","
 
-    -- Use tmux as the target (since tmux is already installed)
-    vim.g.tidal_target = "tmux"
+        -- Use tmux as the target (since tmux is already installed)
+        vim.g.tidal_target = "tmux"
 
-    -- Use ghci via ghcup (default command should work)
-    -- If needed, can customize with: vim.g.tidal_ghci = "ghci"
-  end,
-  config = function()
-    -- Register .tidal file extension
-    vim.filetype.add({
-      extension = {
-        tidal = "tidal",
-      },
-    })
-  end,
+        -- Use ghci via ghcup (default command should work)
+        -- If needed, can customize with: vim.g.tidal_ghci = "ghci"
+    end,
+    config = function()
+        -- Register .tidal file extension
+        vim.filetype.add({
+            extension = {
+                tidal = "tidal",
+            },
+        })
+    end,
 }
